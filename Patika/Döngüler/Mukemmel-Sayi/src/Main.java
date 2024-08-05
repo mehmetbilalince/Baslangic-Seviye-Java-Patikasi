44099ea7272bcc58ce7af8c30c0c1543237cee92
Mehmet Bilal <114563125+mehmetbilalince@users.noreply.github.com>
2024-08-05 23:01:31 +0300

    Add files via upload

---
+++ Patika/Döngüler/Mukemmel-Sayi/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int number = input.nextInt();
+
+        int sum = 0;
+
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+
+        if (sum == number) {
+            System.out.println(number + " Mükemmel sayıdır.");
+        } else {
+            System.out.println(number + " Mükemmel sayı değildir.");
+        }
+    }
+}
